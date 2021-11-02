a6fdcd12f1421e482cb3843c248ad9d1c8f52b18
Abdulrahman <emadabdo258@gmail.com>
2021-11-02 10:46:20 +0200

    first-commit

---
+++ JS/custom.js
@@ -0,0 +1,40 @@
+/*inot in slider */
+let input = document.querySelector("input");
+input.onfocus = function(){
+   this.style.border = "1px solid #fd5631";
+}
+
+/*services */
+let icon  = document.getElementById("downv");
+let down = document.getElementById("downh")
+icon.onclick = function(){
+   down.classList.toggle("shown")
+}
+
+
+
+/*fire carosal  */
+$(document).ready(function(){
+   $('.owl-carousel').owlCarousel();
+ });
+$('.owl-carousel').owlCarousel({
+   center: true,
+   items:2  ,
+   loop:true,
+   nav:true,
+   margin:20,
+   dots:false,
+   // autoplay:true,
+   autoplayTimeout:2000,
+   responsive:{
+      0:{
+         items:1
+      },
+       600:{
+           items:2
+       },
+       500:{
+          items:1
+       }
+   }
+});
